3f640aaa2bfcba46887809f0cdcc13b2c99bb051
Grotter <grottersha123@gmail.com>
2021-09-27 22:44:38 +0300

    первая версия бота добавила базу, иселючнния для новых пользователей добавимла конФИГИ

--- bot.py
+++ bot.py
@@ -22,52 +22,63 @@ inline_btn_2 = InlineKeyboardButton(text_btn2, callback_data='button2')
 
 inline_kb1 = InlineKeyboardMarkup().add(inline_btn_1, inline_btn_2)
 
-count_1 = 0
-count_2 = 0
 
 
 def create_button(data, count_like, count_dislike):
     kb1 = InlineKeyboardMarkup()
     for i in data['inline_keyboard']:
         button1, button2 = i
-        button1['text'] = button1['text'] + ' ' + str(count_like)
-        button2['text'] = button2['text'] + ' ' + str(count_dislike)
+        button1['text'] = text_btn1 + ' ' + str(count_like)
+        button2['text'] = text_btn2 + ' ' + str(count_dislike)
         kb1.add(InlineKeyboardButton(**button1), InlineKeyboardButton(**button2))
     return kb1
+
+def statistic_update(db, message_id):
+    count = db.get(str.encode(message_id))
+    count = int(count or 0) + 1
+    db.put(str.encode(message_id), str.encode(str(count)))
+    return count
+
+
+def get_count(db, message_id):
+    count = db.get(str.encode(message_id))
+    return int(count or 0)
 
 
 @dp.callback_query_handler(lambda c: c.data and c.data.startswith('button'))
 async def process_callback_kb1btn1(callback_query: types.CallbackQuery):
     global count_1
     global count_2
+    global inline_kb1
     code = callback_query.data[-1]
-    print(callback_query.message.message_id, callback_query.from_user.username, callback_query.message.chat.id)
-    data = inline_kb1.as_json()
-    kb1 = InlineKeyboardMarkup()
-    data = ast.literal_eval(data)
-    for i in data['inline_keyboard']:
-        button1, button2 = i
-        kb1.add(InlineKeyboardButton(**button1), InlineKeyboardButton(**button2))
-    print(kb1)
+    message_id = callback_query.message.message_id
+    user = callback_query.from_user.username
     if code.isdigit():
         code = int(code)
-    if code == 1:
-        # kb1.add(InlineKeyboardButton(**button1), InlineKeyboardButton(**button2))
-        count_1 += 1
-    if code == 2:
-        count_2 += 1
-    else:
-        await bot.answer_callback_query(callback_query.id)
-    kb1 = create_button(data, count_1, count_2)
-    await bot.edit_message_reply_markup(message_id=callback_query.message.message_id,
-                                        chat_id=callback_query.message.chat.id, reply_markup=kb1)
+    users = db.get(str.encode(str(message_id)+'u'))
+    if user not in str(users or ''):
+        db.put(str.encode(str(message_id)+'u'), str.encode(str(users or '')+'_'+user))
+        if code == 1:
+            count_l = statistic_update(db, str(message_id)+'l')
+            count_d = get_count(db, str(message_id)+'d')
+            # kb1.add(InlineKeyboardButton(**button1), InlineKeyboardButton(**button2))
+        if code == 2:
+            count_d = statistic_update(db, str(message_id) + 'd')
+            count_l = get_count(db, str(message_id)+'l')
+
+        data = inline_kb1.as_json()
+        data = ast.literal_eval(data)
+        inline_kb1 = create_button(data, count_l, count_d)
+
+        await bot.edit_message_reply_markup(message_id=callback_query.message.message_id,
+                                        chat_id=callback_query.message.chat.id, reply_markup=inline_kb1)
+    await bot.answer_callback_query(callback_query.id, text='вы уже голосовали')
 
 
 @dp.message_handler()
 async def any_text_message(message: types.Message):
-    print(message.message_id)
-    # if not message.from_user.is_bot:
-    await message.reply(message.text, reply_markup=inline_kb1)
+    if not message.from_user.is_bot:
+        await message.reply(message.text, reply_markup=inline_kb1)
 
 
 if __name__ == "__main__":
